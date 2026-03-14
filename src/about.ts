bbfc597c6d0b7898833322d314d11b36e7d9b4b2
Ethan Liu <eliu3152@gmail.com>
2026-03-13 22:42:37 -0400

    Start work on styling menu page

--- src/about.ts
+++ src/about.ts
@@ -4,16 +4,13 @@ function writeAboutPage() {
     const storySection = document.createElement("div");
     storySection.classList.add("about-section");
     const storyTitle = document.createElement("h2");
-    storyTitle.classList.add("about-section-title");
+    storyTitle.classList.add("section-title");
     storyTitle.textContent = "Our Story";
     const storyBody = document.createElement("p");
     storyBody.classList.add("description");
     storyBody.textContent = `Founded in 2026, Jimbo was fishing and caught a hammerhead. For some reason
-    he decided to cook it... The next week he cooked a whale... octopus... shrimp... And his boat became a restaurant.`
+    he decided to cook it... Now he runs a restaurant :).`
     storySection.append(storyTitle, storyBody);
-
-
-
     homepage?.append(storySection);
 }
 
--- src/menu.ts
+++ src/menu.ts
@@ -1,5 +1,60 @@
-function writeMenuPage() {
+import shrimpImg from "./img/shrimp.png";
+import lobsterImg from "./img/lobster.png";
+import crabImg from "./img/crab.png";
 
+const homepage = document.getElementById("content");
+
+type menuItem = {img: string, name: string, price: number}
+
+function createMenuSubsection(title: string, items: menuItem[]) {
+    const section = document.createElement("div");
+    section.classList.add("menu-subsection");
+    const sectionHeader = document.createElement("div");
+    sectionHeader.textContent = title;
+    sectionHeader.classList.add("menu-subsection-header");
+
+    const sectionList = document.createElement("div");
+    sectionList.classList.add("menu-subsection-list");
+    for (let item of items) {
+        const menuEntry = document.createElement("div");
+        menuEntry.classList.add("menu-entry");
+        const img = document.createElement("img");
+        img.src = item.img;
+        const name = document.createElement("h4");
+        name.textContent = item.name;
+        const price = document.createElement("p");
+        price.textContent = "$" + item.price;
+        menuEntry.append(img, name, price);
+        sectionList.append(menuEntry);
+    }
+    section.append(sectionHeader, sectionList);
+    return section;
+}
+
+function writeMenuPage() {
+    const menuTitle = document.createElement("h2");
+    menuTitle.classList.add("section-title");
+    menuTitle.textContent = "Menu";
+
+    const menuDisplay = document.createElement("div");
+    menuDisplay.classList.add("menu-display");
+
+    const appetizerItems:menuItem[] = [];
+    const entreeItems:menuItem[] = [
+        {img: shrimpImg, name: "Infinite shrimp", price: 99.9},
+        {img: lobsterImg, name: "Juicy lobster", price: 199.9},
+        {img: crabImg, name: "JUMBO crabs", price: 999.9}
+    ];
+    const dessertItems:menuItem[] = [];
+    const drinkItems:menuItem[] = [];
+
+    const appetizers = createMenuSubsection("Appetizers", appetizerItems);
+    const entrees = createMenuSubsection("Entrees", entreeItems);
+    const desserts = createMenuSubsection("Desserts", dessertItems);
+    const drinks = createMenuSubsection("Drinks", drinkItems);
+    
+    menuDisplay.append(appetizers, entrees, desserts, drinks);
+    homepage?.append(menuTitle, menuDisplay);
 }
 
 export default writeMenuPage;
